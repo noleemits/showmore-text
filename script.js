fc279baf401ec9b7c482bf2e361d066e94ec0cb9
noleemits <45466598+noleemits@users.noreply.github.com>
2022-12-14 07:23:31 -0600

    Added comments about how to use the script

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+//look for the the required classe the one that will be clicked to extend and the one that will be extended
 const viewToggle = document.querySelectorAll(".view-toggle");
 const toggleText = document.querySelectorAll(".toggle-text");
 
 viewToggle.forEach((toggleSingular) => {
     toggleSingular.addEventListener("click", () => {
+      //The view more or be less need to be placed at the end of the paragraph, and the paragraph should be set to none initially  
       const panel = toggleSingular.previousElementSibling;
+      //you need to create a class active and set it to display block, inline, or inline-block  
       panel.classList.toggle("active");
       let txt =  toggleSingular.innerText;
       toggleSingular.innerText = txt == '…View more' ? ' ...View less' : '…View more';
